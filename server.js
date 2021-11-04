1a8cd5b3e57579f1614f4a6ce2ec6bd47dd7cbf7
MaxHuw <m.huw.graham@gmail.com>
2021-11-04 14:24:03 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const httpServer = http.createServer(app);
+const io = new Server(httpServer);
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
+
+io.on("connection", (socket) => {
+  console.log(`New client connected`);
+});
